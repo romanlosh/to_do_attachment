d5d48b65d6f3493dc617c249b9fa1b3aac2ab92c
Roman Losh <43208233+romanlosh@users.noreply.github.com>
2019-01-10 16:38:05 +0200

    . . .

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(function() {
+	var tasksList = $('#tasksList');
+	var taskInput = $('#taskInput');
+	var notification = $('#notification');
+
+
+	var displayNotification = function() {
+		if (!tasksList.children().length) {
+			notification.fadeIn('fast');
+		} else {
+			notification.css('display', 'none')
+		}
+	}
+
+	$('#taskAdd').on('click', function() {
+
+		if (!taskInput.val()) {return false;}
+		tasksList.append("<li>" + taskInput.val() + "<button class='delete'>&#10006</button></li>");
+
+		taskInput.val("");
+
+		displayNotification();
+
+		$('.delete').on('click', function() {
+			var $parent = $(this).parent();
+
+			$parent.fadeOut();
+
+			setTimeout(function() {
+				$parent.remove();
+				displayNotification();
+			}, 300);
+		})
+	})
+
+})
